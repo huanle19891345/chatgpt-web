0230b49c4ff1b0ebf0dae21a838d5009ea401bbb
zhenghuan <huanle19891345@126.com>
2023-04-19 21:13:03 +0800

    fix: add compression

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import compression from 'compression'
 import type { RequestProps } from './types'
 import type { ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
@@ -8,8 +9,8 @@ import { isNotEmptyString } from './utils/is'
 
 const app = express()
 const router = express.Router()
-
-app.use(express.static('publics'))
+app.use(compression())
+app.use(express.static('public'))
 app.use(express.json())
 
 app.all('*', (_, res, next) => {
